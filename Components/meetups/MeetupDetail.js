13bc58c3f10c3dda46d4726835640818ade25fc4
Stefano-Vitelli <stefanovitelli9@gmail.com>
2023-01-13 20:27:30 +0100

    test commit

--- Components/meetups/MeetupDetail.js
+++ Components/meetups/MeetupDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import classes from './MeetupDetail.module.css';
+import classes from "./MeetupDetail.module.css";
 
 const MeetupDetail = (props) => {
   return (
